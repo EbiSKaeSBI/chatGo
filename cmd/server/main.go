83e42d3061207ee6765534b1b8703badcd3d6658
Vladimir <zajtzeffvl4d@yandex.ru>
2026-03-11 02:36:24 +0300

    Added: Добавил websocket, но не доделал

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,30 +1,80 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/websocket"
+)
 
 func main() {
 
 	mux := http.NewServeMux()
 
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
 		}
+		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte("Ok! Server is up and running!"))
 		if err != nil {
 			return
 		}
+
 	})
 
+	fileServer := http.FileServer(http.Dir("./web/styles"))
+	mux.Handle("/styles/", http.StripPrefix("/styles", fileServer))
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		http.ServeFile(w, r, "./index.html")
+		if r.Method != "GET" {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		http.ServeFile(w, r, "./web/index.html")
+	})
+
+	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		q := r.URL.Query()
+		username := strings.TrimSpace(q.Get("username"))
+
+		if r.Method != "GET" {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		if username == "" {
+			http.Error(w, "Missing username", http.StatusBadRequest)
+			return
+		}
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer func(conn *websocket.Conn) {
+			err := conn.Close()
+			if err != nil {
+
+			}
+		}(conn)
+		log.Println("websocket connected:", username)
+		conn.WriteMessage(1, []byte("ok"))
 	})
 
 	addr := ":8080"
 
+	log.Println("Listening on", addr)
 	err := http.ListenAndServe(addr, mux)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 }
